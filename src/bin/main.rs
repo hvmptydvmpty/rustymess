182159530ba79229d7ed79e37b37000cac87a3a9
Sergey Lipnevich <43814+hvmptydvmpty@users.noreply.github.com>
2021-04-11 15:36:21 -0400

    rand works with edition=2018 ordering formatting

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,21 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    let mut guess = String::new();
+
+    println!("Guess the number!");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    guess = guess.trim().to_string(); // just to see how it's done
+    let guess: u32 = guess.parse().expect("Please type a number!");
+
+    //println!("You guessed: {}", guess);
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
