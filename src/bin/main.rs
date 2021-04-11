bd1fbdcd397f3109bac768fb6a3c80896cc45b1a
hvmptydvmpty <>
2021-04-11 21:00:23 +0000

    Learn to use match; auto-format in replit

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -5,17 +5,27 @@ use std::io;
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
+        println!("Guess the number?");
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        guess = guess.trim().to_string(); // just to see how it's done
+        let guess: u32 = match guess.parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Not a number!");
+                continue;
+            }
+        };
 
-    println!("Guess the number!");
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    guess = guess.trim().to_string(); // just to see how it's done
-    let guess: u32 = guess.parse().expect("Please type a number!");
-
-    //println!("You guessed: {}", guess);
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        //println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
